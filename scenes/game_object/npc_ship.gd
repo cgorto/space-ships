9484cbf4cd7945e43628e53c15be62145dd681bf
cgorto <christian.gorton@colorado.edu>
2024-10-20 17:14:06 -0600

    enemies

---
+++ scenes/game_object/npc_ship.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+
+@onready var ship_physics: ShipPhysics = $ShipPhysics
+@onready var ship_input: Node3D = $ShipInput
+
+var velocity: Vector3
+
+func _process(_delta: float) -> void:
+	ship_physics.set_physics_input(
+		Vector3(
+			ship_input.strafe, 
+			0, 
+			-ship_input.throttle
+		),
+		Vector3(
+			ship_input.pitch,
+			ship_input.yaw,
+			ship_input.roll
+		)
+	)
--- scenes/test_scene.gd
+++ scenes/test_scene.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var box: PackedScene = preload("res://scenes/csg_mesh_3d.tscn")
+@onready var enemy: PackedScene = preload("res://scenes/game_object/npc_ship.tscn")
 
 @onready var debug_cam: Node3D = $CameraRig/CanvasLayer/SubViewportContainer/SubViewport/Cam
 
@@ -10,6 +11,11 @@ func _ready() -> void:
 		var new_box: Node3D = box.instantiate()
 		new_box.position = Vector3(box_scale *randf() -box_scale*0.5, box_scale *randf() -box_scale*0.5, box_scale *randf() -box_scale*0.5)
 		add_child(new_box)
+	for i in 100:
+		var new_npc: Node3D = enemy.instantiate()
+		new_npc.position = Vector3(box_scale *randf() -box_scale*0.5, box_scale *randf() -box_scale*0.5, box_scale *randf() -box_scale*0.5)
+		add_child(new_npc)
+		new_npc.ship_input.target = $Ship
 
 func _physics_process(_delta: float) -> void:
 	debug_cam.position = $Ship.position
