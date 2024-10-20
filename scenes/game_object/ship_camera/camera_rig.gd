8108e1bcfe8ff9499288788b73d10916820598b6
David Gorton <32966014+notrogd@users.noreply.github.com>
2024-10-19 19:57:00 -0600

    Rotates instead of Basis

--- scenes/game_object/ship_camera/camera_rig.gd
+++ scenes/game_object/ship_camera/camera_rig.gd
@@ -5,9 +5,9 @@ class_name CameraRig extends Node3D
 @export var fancy_cam: bool
 
 @export_category("Lookahead Values")
-@export var horizontal_turn_angle: float = deg_to_rad(25)
-@export var vertical_turn_up_angle: float = deg_to_rad(60)
-@export var vertical_turn_down_angle: float = deg_to_rad(45)
+@export var horizontal_turn_angle: float = deg_to_rad(-20)
+@export var vertical_turn_up_angle: float = deg_to_rad(30)
+@export var vertical_turn_down_angle: float = deg_to_rad(20)
 
 @onready var camera: Node3D = $LookAheadRig/MainCamera/Camera3D
 @onready var look_ahead_rig: Node3D = $LookAheadRig
@@ -37,22 +37,23 @@ func look_ahead(delta:float) -> void:
 	
 	var mouse_screen: Vector2 = (mouse_pos - (screen_size * 0.5)) / (screen_size * 0.5)
 	mouse_screen = mouse_screen.clampf(-1,1)
-	
 	var horizontal: float = horizontal_turn_angle * mouse_screen.x
 	var vertical: float = vertical_turn_down_angle * mouse_screen.y if mouse_screen.y < 0.0 else vertical_turn_up_angle * mouse_screen.y
 
-	var target_rotation: Basis = Basis.from_euler(Vector3(-vertical,-horizontal,0))
-	look_ahead_rig.basis = look_ahead_rig.basis.slerp(target_rotation, smooth_speed * delta)
+	#var target_rotation: Basis = Basis.from_euler(Vector3(vertical,horizontal,0),0)
+	#look_ahead_rig.basis = look_ahead_rig.basis.slerp(target_rotation, smooth_speed * delta)
 
+	look_ahead_rig.rotate(Vector3.BACK, vertical * delta)
+	look_ahead_rig.rotate(Vector3.RIGHT, horizontal * delta)
 	var up: Vector3 = global_transform.basis.y
-
 
 	var lookahead_pos: Vector3 = ship.global_position-(ship.global_transform.basis.z * 100)
 	$LookAheadRig/MainCamera/Camera3D/RayCast3D.target_position = camera.to_local(lookahead_pos)
-	counter += delta
-	if counter > 1:
-		print("Look ahead: " + str(-ship.global_transform.basis.z * 100))
-		print("THIS ONE" + str(lookahead_pos))
-		counter = 0
-	camera.look_at(lookahead_pos)
-	#camera.basis = camera.basis.looking_at(lookahead_pos, up)
+	#counter += delta
+	#if counter > .5:
+		#look_ahead_rig.basis = look_ahead_rig.basis.orthonormalized()
+		#counter=0.0
+
+	#camera.look_at(lookahead_pos, camera.basis.y)
+	camera.look_at(lookahead_pos, look_ahead_rig.basis.y)
+	#camera.basis = camera.basis.looking_at(lookahead_pos, global_basis.y)
